c045f4866aecf7e7001618d050b59a1f7edb4878
uehddms <ehddms3@sookmyung.ac.kr>
2024-11-11 21:31:09 +0900

    add template

--- diary/src/main.jsx
+++ diary/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {" "}
-    {/* 개발안정성 검증 mode */}
     <App />
   </StrictMode>
 );
